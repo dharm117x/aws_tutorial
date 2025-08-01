b758a89e25121471145e459fb48990e195344c4b
Dharmendra Kuamr <34061027+dharm117x@users.noreply.github.com>
2025-08-01 19:14:07 +0000

    Add security group

--- terraform/Lab1/main.tf
+++ terraform/Lab1/main.tf
@@ -2,7 +2,16 @@ provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "terrform-ec2" {
     ami           = "ami-0c7f981fd31e5c7ae"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+}
+
+data "aws_security_group" "existing_sg" {
+      id = "sg-0564146373b75e1cc" # Replace with your security group ID
+      # Alternatively, use filters to find it:
+      # name = "my-existing-security-group"
+      # tags = {
+      #   Environment = "production"
+      # }
 }
